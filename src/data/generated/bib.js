30384685829c41b0da0be4c32b61f75db93152ba
Cobalt010 <wuhuuuuu00@gmail.com>
2026-05-11 01:18:06 +0800

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,102 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
+const generatedBibEntries = {
+  "frazier2018tutorial": {
+    "abstract": "Bayesian optimization is an approach to optimizing objective functions that take a long time to evaluate. It builds a surrogate for the objective and quantifies the uncertainty using Gaussian process regression, then uses an acquisition function to decide where to sample next.",
+    "author": "Frazier, Peter I.",
+    "doi": "10.1287/educ.2018.0188",
+    "keywords": "bayesian optimization, gaussian process, acquisition function, foundations",
+    "series": "TutORials in Operations Research",
+    "title": "A Tutorial on Bayesian Optimization",
+    "type": "article",
+    "year": "2018"
+  },
+  "wang2023recent": {
+    "abstract": "A comprehensive survey of recent advances in Bayesian optimization, categorising existing work into nine main groups and discussing open problems.",
+    "author": "Wang, Xilu and Jin, Yaochu and Schmitt, Sebastian and Olhofer, Markus",
+    "doi": "10.1145/3582078",
+    "keywords": "bayesian optimization, survey, gaussian process, acquisition function",
+    "series": "ACM Computing Surveys",
+    "title": "Recent Advances in Bayesian Optimization",
+    "type": "article",
+    "year": "2023"
+  },
+  "bergstra2012random": {
+    "abstract": "Shows that random search is more efficient than grid search for hyperparameter optimization.",
+    "author": "Bergstra, James and Bengio, Yoshua",
+    "doi": "10.5555/2188385.2188395",
+    "keywords": "hyperparameter optimization, random search, grid search, baseline",
+    "series": "JMLR",
+    "title": "Random Search for Hyper-Parameter Optimization",
+    "type": "article",
+    "year": "2012"
+  },
+  "snoek2012practical": {
+    "abstract": "Applies Bayesian optimization to hyperparameter tuning using a Gaussian process surrogate with Expected Improvement.",
+    "author": "Snoek, Jasper and Larochelle, Hugo and Adams, Ryan P.",
+    "doi": "10.48550/arXiv.1206.2944",
+    "keywords": "hyperparameter optimization, bayesian optimization, gaussian process, machine learning",
+    "series": "NeurIPS",
+    "title": "Practical Bayesian Optimization of Machine Learning Algorithms",
+    "type": "article",
+    "year": "2012"
+  },
+  "kandasamy2018neural": {
+    "abstract": "Extends Bayesian optimization to neural architecture search using Optimal Transport to define a distance metric between architectures.",
+    "author": "Kandasamy, Kirthevasan and Neiswanger, Willie and Schneider, Jeff and Poczos, Barnabas and Xing, Eric P.",
+    "doi": "10.48550/arXiv.1802.07191",
+    "keywords": "neural architecture search, bayesian optimization, optimal transport, deep learning",
+    "series": "NeurIPS",
+    "title": "Neural Architecture Search with Bayesian Optimisation and Optimal Transport",
+    "type": "article",
+    "year": "2018"
+  },
+  "falkner2018bohb": {
+    "abstract": "Combines Bayesian optimization with Hyperband to achieve strong anytime performance for large-scale hyperparameter optimization.",
+    "author": "Falkner, Stefan and Klein, Aaron and Hutter, Frank",
+    "doi": "10.48550/arXiv.1807.01774",
+    "keywords": "hyperparameter optimization, bayesian optimization, hyperband, scalable",
+    "series": "ICML",
+    "title": "BOHB: Robust and Efficient Hyperparameter Optimization at Scale",
+    "type": "article",
+    "year": "2018"
+  },
+  "bai2023transfer": {
+    "abstract": "Surveys transfer learning methods for Bayesian optimization, addressing the cold-start problem.",
+    "author": "Bai, Tengfei and Li, Yue and Shen, Yanqi and Zhang, Xueqian and Zhang, Wei and Cui, Bin",
+    "doi": "10.48550/arXiv.2302.05927",
+    "keywords": "transfer learning, bayesian optimization, cold start, meta-learning",
+    "series": "arXiv",
+    "title": "Transfer Learning for Bayesian Optimization: A Survey",
+    "type": "article",
+    "year": "2023"
+  },
+  "paria2020flexible": {
+    "abstract": "Proposes a multi-objective Bayesian optimization framework using random scalarizations to approximate the Pareto front.",
+    "author": "Paria, Biswajit and Kandasamy, Kirthevasan and Poczos, Barnabas",
+    "doi": "10.48550/arXiv.1805.12168",
+    "keywords": "multi-objective optimization, bayesian optimization, pareto front, scalarization",
+    "series": "UAI",
+    "title": "A Flexible Framework for Multi-Objective Bayesian Optimization using Random Scalarizations",
+    "type": "article",
+    "year": "2020"
+  },
+  "hellan2023climate": {
+    "abstract": "Reviews applications of Bayesian optimization to climate change problems with new benchmarks.",
+    "author": "Hellan, Sigrid Passano and Lucas, Christopher G. and Goddard, Nigel H.",
+    "doi": "10.48550/arXiv.2306.04343",
+    "keywords": "bayesian optimization, climate change, environmental monitoring, applications",
+    "series": "arXiv",
+    "title": "Bayesian Optimisation Against Climate Change: Applications and Benchmarks",
+    "type": "article",
+    "year": "2023"
+  },
+  "kotthoff2021materials": {
+    "abstract": "Surveys the use of Bayesian optimization in materials science for experimental design.",
+    "author": "Kotthoff, Lars and Wahab, Hud and Johnson, Patrick A.",
+    "doi": "10.48550/arXiv.2108.00002",
+    "keywords": "bayesian optimization, materials science, experimental design, applications",
+    "series": "arXiv",
+    "title": "Bayesian Optimization in Materials Science: A Survey",
+    "type": "article",
+    "year": "2021"
+  }
 };
